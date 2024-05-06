14808effbf7e0127b0ac89b474e0d194e8febbdb
bhoomi <bhoomi1414agarwal@gmail.com>
2024-05-06 12:54:26 +0530

    update

--- server/keys.js
+++ server/keys.js
@@ -1,11 +1,13 @@
 module.exports={
     MONGO_URL:"mongodb+srv://Bhoomiii:fJThvFuM6W2Y6IUf@cluster0.mby29ww.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET:"efffreyjnhjj6",
-    EMAIL_HOST:'smtp.gmail.com',
+    EMAIL_HOST:smtp.gmail.com,
     EMAIL_PORT:465,
     EMAIL_USER:'bhoomi1414agarwal@gmail.com',
     EMAIL_PASS:'gzjt yzox ctkd bbss',
     EMAIL_FROM:'bhoomi1414agarwal@gmail.com'
 }
+
+
 
 
